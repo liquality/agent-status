f3fa1d79fd6c02caf4966def2782ecbbaab585ac
Harsh Vakharia <harshjv@gmail.com>
2020-09-30 19:54:27 +0530

    add uni

--- src/App.svelte
+++ src/App.svelte
@@ -11,7 +11,8 @@ const currencyMap = {
   DAI: 1e18,
   USDC: 1e6,
   USDT: 1e6,
-  WBTC: 1e8
+  WBTC: 1e8,
+  UNI: 1e18
 }
 const currencyDecimalMap = {
   BTC: 1e6,
@@ -19,7 +20,8 @@ const currencyDecimalMap = {
   DAI: 1e2,
   USDC: 1e2,
   USDT: 1e2,
-  WBTC: 1e6
+  WBTC: 1e6,
+  UNI: 1e6
 }
 const results = {}
 const totalUSD = {}
@@ -62,6 +64,11 @@ const MINMAX = {
     min: 5,
     target: 10,
     max: 15
+  },
+  UNI: {
+    min: 1,
+    target: 2,
+    max: 3
   }
 }
 
@@ -141,7 +148,7 @@ async function run (network, env) {
 }
 
 async function updateUSD () {
-  const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum,bitcoin,dai,usd-coin,tether,wrapped-bitcoin&vs_currencies=usd')
+  const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum,bitcoin,dai,usd-coin,tether,wrapped-bitcoin,uniswap&vs_currencies=usd')
   const assets = await response.json()
 
   USD.BTC = assets.bitcoin.usd
@@ -150,6 +157,7 @@ async function updateUSD () {
   USD.USDC = assets['usd-coin'].usd
   USD.USDT = assets.tether.usd
   USD.WBTC = assets['wrapped-bitcoin'].usd
+  USD.UNI = assets.uniswap.usd
 
   setTimeout(updateUSD, random(15000, 60000))
 }
